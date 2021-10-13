6e7ccf5ab7025c60497ee227644da0288e56b3e0
Juan Víctor Jiménez Corzo <corzo@outlook.com>
2021-10-13 11:48:32 -0500

    chages in packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(
-    name="lambda_router",
-    version="0.1",
-    description="Simple router for lambda projects",
-    author="Víctor Jiménez",
-    author_email="corzo@outlook.com",
-    url="https://github.com/jvjc/python-lambda-router",
-)
+if __name__ == "__main__":
+    setuptools.setup()
