5de02803a1b9112a9405d5db9112a923725008b3
Juan Víctor Jiménez Corzo <corzo@outlook.com>
2021-10-13 11:28:12 -0500

    changes in dir

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(
-    name="LambdaRouter",
+    name="lambda_router",
     version="0.1",
     description="Simple router for lambda projects",
     author="Víctor Jiménez",
