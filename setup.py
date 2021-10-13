2115f458718703ac8ce58b3db76044dc558b7520
Juan Víctor Jiménez Corzo <corzo@outlook.com>
2021-10-13 11:20:44 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="LambdaRouter",
+    version="0.1",
+    description="Simple router for lambda projects",
+    author="Víctor Jiménez",
+    author_email="corzo@outlook.com",
+    url="https://github.com/jvjc/python-lambda-router",
+    packages=["lambda-router"],
+)
